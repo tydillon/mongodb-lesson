8bd5fe652acf02c2426fcb31a4c4e58957b89805
Tyler Dillon <harrell_t@icloud.com>
2019-06-26 11:44:48 -0400

    moving routes to own file

--- api/routes/students.js
+++ api/routes/students.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const Student = require('../models/Student')
-const router = express.Router
+const router = express.Router()
 
 // /students
 router
@@ -10,13 +10,40 @@ router
       res.json({ status: 'ok', data: students })
     })
   })
-  .post(function(req, res) {})
+  .post(function(req, res) {
+    const rawStudent = req.body
+
+    const newStudent = new Student(rawStudent)
+    //this stores it in the database
+    newStudent.save()
+    //can do newStudent: newStudent
+    res.json({ status: 'ok', newStudent })
+  })
 
 // /students/:studentId
 router
   .route('/:studentId')
-  .get(function(req, res) {})
-  .put(function(req, res) {})
-  .delete(function(req, res) {})
+  .get(function(req, res) {
+    Student.findById(req.params.studentId).then(function(foundStudent) {
+      res.json(foundStudent)
+    })
+  })
+  .put(function(req, res) {
+    Student.findById(req.params.studentId).then(function(foundStudent) {
+      foundStudent.name = req.body.name
+      foundStudent.age = req.body.age
+      foundStudent.photoUrl = req.body.photoUrl
+      foundStudent.bio = req.body.bio
+
+      foundStudent.save()
+
+      res.json(foundStudent)
+    })
+  })
+  .delete(function(req, response) {
+    Student.findByIdAndDelete(req.params.studentId).then(function(res) {
+      response.json({ status: 'ok', res: res })
+    })
+  })
 
 module.exports = router
--- api/server.js
+++ api/server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const Student = require('./models/Student')
-// const StudentRoutes = require('./routes/students,js')
+const StudentRoutes = require('./routes/students.js')
 
 mongoose.connect('mongodb://localhost:27017/students', {
   useNewUrlParser: true
@@ -11,62 +11,7 @@ mongoose.connect('mongodb://localhost:27017/students', {
 
 const app = express()
 app.use(bodyParser())
-// app.use('/students', StudentRoutes)
-
-//define the route, then provide the
-//req= incoming, res= outgoing
-//Main GET / route
-app.get('/', function(req, res) {
-  // if you don't use the res, the browser will just spin because the app isn't 'getting' anything
-  res.json({ status: 'ok', message: 'Welcome to my server hullo' })
-})
-
-app.get('/students', function(req, res) {
-  //Student connects to Mongo. Find is pulling it out from Mongo, then students comes back
-  //Server sends it back as JSON to the browser
-  Student.find({}).then(function(students) {
-    res.json({ status: 'ok', data: students })
-  })
-})
-
-//this is hard coded. We want to change this
-app.post('/students', function(req, res) {
-  const rawStudent = req.body
-
-  const newStudent = new Student(rawStudent)
-  //this stores it in the database
-  newStudent.save()
-  //can do newStudent: newStudent
-  res.json({ status: 'ok', newStudent })
-})
-
-//querying the database by id, finding student, sending it back
-app.get('/students/:studentId', function(req, res) {
-  Student.findById(req.params.studentId).then(function(foundStudent) {
-    res.json(foundStudent)
-  })
-})
-
-//update a student by id
-app.put('/students/:studentId', function(req, res) {
-  Student.findById(req.params.studentId).then(function(foundStudent) {
-    foundStudent.name = req.body.name
-    foundStudent.age = req.body.age
-    foundStudent.photoUrl = req.body.photoUrl
-    foundStudent.bio = req.body.bio
-
-    foundStudent.save()
-
-    res.json(foundStudent)
-  })
-  //   Student.update(req.params.studentId)
-})
-
-app.delete('/students/:studentId', function(req, response) {
-  Student.findByIdAndDelete(req.params.studentId).then(function(res) {
-    response.json({ status: 'ok', res: res })
-  })
-})
+app.use('/students', StudentRoutes)
 
 //turns the app on
 app.listen(8000, function() {
