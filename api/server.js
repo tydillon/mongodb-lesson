ade1dd0602bb0518cafb01a9105848b35671495a
Tyler Dillon <harrell_t@icloud.com>
2019-06-26 12:47:55 -0400

    midway through front end functionality with CORS error

--- api/server.js
+++ api/server.js
@@ -1,5 +1,6 @@
 //framework to create a server in node. Lets you create routes
 const express = require('express')
+const cors = require('cors')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const Student = require('./models/Student')
@@ -9,9 +10,11 @@ mongoose.connect('mongodb://localhost:27017/students', {
   useNewUrlParser: true
 })
 
+//SOMETHING WRONG WITH CORS
 const app = express()
 app.use(bodyParser())
 app.use('/students', StudentRoutes)
+app.use(cors())
 
 //turns the app on
 app.listen(8000, function() {
--- web/main.js
+++ web/main.js
@@ -1,16 +1,51 @@
-// iife = immediately invoked function expression. keeps all of your variables from being global
-;(function() {})()
-
 const page = {
+  //things we want to happen on pageload
+  init: function() {
+    page.getStudents().then(function(students) {
+      console.log('STUDENTS', students)
+      page.addStudentsToPage(students.data)
+    })
+    page.initEvents()
+  },
+  initEvents: function() {
+    page.clickDetails()
+  },
+  clickDetails: function() {
+    document.addEventListener('click', function(e) {
+      e.preventDefault()
+      //checking to see what was actually grabbed
+      //   console.log(e.target)
+    })
+  },
+  //other things
   getStudents: function() {
     return fetch('http://localhost:8000/students')
       .then(function(students) {
         return students.json()
       })
       .catch(e => console.log('Error', e))
+  },
+  addStudentsToPage: function(students) {
+    let html = '<ul>'
+    students.forEach(function(student) {
+      html += `<li>${page.singleStudentTemplate(student)}</li>`
+    })
+    html += '</ul>'
+    const $studentsList = document.querySelector('#studentList')
+    $studentsList.innerHTML = html
+    console.log(html)
+  },
+  singleStudentTemplate: function(student) {
+    return `<div data-id=${student._id}>
+    <h3>${student.name}</h3>
+    <img src="${student.photoUrl}">
+    <p>${student.bio}</p>
+    <a class="detailLink" href="${student._id}">More Details</a>
+    </div>`
   }
-}
-
-page.getStudents().then(function(students) {
-  console.log('STUDENTS', students)
-})
+}(
+  // iife = immediately invoked function expression. keeps all of your variables from being global
+  function() {
+    page.init()
+  }
+)()
